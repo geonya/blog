ff6291a77f5c5fd75625e9ee64edc5edd8a69c4e
geonya <geony8410@gmail.com>
2022-04-23 23:04:57 +0900

    prettier reset

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Posts' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
-]
+	{ href: "/blog", title: "Posts" },
+	{ href: "/projects", title: "Projects" },
+	{ href: "/tags", title: "Tags" },
+	{ href: "/about", title: "About" },
+];
 
-export default headerNavLinks
+export default headerNavLinks;
