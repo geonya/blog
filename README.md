52f9fbc4d504ef301870293e0f7937e8ca91360a
geonya <geony8410@gmail.com>
2022-04-23 17:19:39 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Geonys Blog
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Posts' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
