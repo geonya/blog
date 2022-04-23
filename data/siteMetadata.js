99d4c5eb00e6eb2ae41c1e012eb41d66aa0398d4
geonya <geony8410@gmail.com>
2022-04-24 00:50:11 +0900

    update sitemetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,11 +5,11 @@ const siteMetadata = {
 	description: "Learning & Building",
 	language: "en-us",
 	theme: "system", // system, dark or light
-	siteUrl: "https://tailwind-nextjs-starter-blog.vercel.app",
+	siteUrl: "https://blog-geonya.vercel.app/",
 	siteRepo: "https://github.com/geonya/blog",
 	siteLogo: "/static/images/logo.png",
 	image: "/static/images/avatar.png",
-	socialBanner: "/static/images/twitter-card.png",
+	socialBanner: "/static/images/time-machine.jpg",
 	email: "geony8410@gmail.com",
 	github: "https://github.com/geonya",
 	twitter: "https://twitter.com/Twitter",
