dd5292d35d8959dc68e267ef5524914e732e08de
geonya <geony8410@gmail.com>
2022-04-23 21:22:07 +0900

    giscus setup4

--- components/comments/Giscus.tsx
+++ components/comments/Giscus.tsx
@@ -32,16 +32,16 @@ const Giscus = () => {
 
     const script = document.createElement('script')
     script.src = 'https://giscus.app/client.js'
-    script.setAttribute('data-repo', 'geonya/blog')
-    script.setAttribute('data-repo-id', 'R_kgDOHOOzuQ')
-    script.setAttribute('data-category', 'Announcements')
-    script.setAttribute('data-category-id', 'DIC_kwDOHOOzuc4COvJm')
-    script.setAttribute('data-mapping', mapping)
-    script.setAttribute('data-reactions-enabled', reactions)
-    script.setAttribute('data-emit-metadata', metadata)
-    script.setAttribute('data-input-position', inputPosition)
-    script.setAttribute('data-lang', lang)
-    script.setAttribute('data-theme', commentsTheme)
+    script.setAttribute('data-repo', 'geonya/blog-comments')
+    script.setAttribute('data-repo-id', 'R_kgDOHOSC1w')
+    script.setAttribute('data-category', 'comments')
+    script.setAttribute('data-category-id', 'DIC_kwDOHOSC184COvTx')
+    script.setAttribute('data-mapping', 'pathname')
+    script.setAttribute('data-reactions-enabled', '1')
+    script.setAttribute('data-emit-metadata', '0')
+    script.setAttribute('data-input-position', 'top')
+    script.setAttribute('data-lang', 'en')
+    script.setAttribute('data-theme', 'dark')
     script.setAttribute('crossorigin', 'anonymous')
     script.async = true
 
