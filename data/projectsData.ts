8682cfd2d704ea592058886262fa608f7c0361d1
geonya <geony8410@gmail.com>
2022-04-28 20:58:57 +0900

    220428

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,17 +1,21 @@
 const projectsData = [
 	{
-		title: "A Search Engine",
-		description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-		imgSrc: "/static/images/google.png",
-		href: "https://www.google.com",
+		title: "Youtube Clone : Bbom",
+		description: `NodeJS, Express, Pug, SCSS, MongoDB, heroku, AWS S3`,
+		imgSrc: "/static/images/bbom.png",
+		href: "https://bbom2022.herokuapp.com/",
 	},
 	{
-		title: "Build the blog",
-		description: `Build NextJs + TailwindCSS Blog`,
-		imgSrc: "/static/images/time-machine.jpg",
-		href: "/blog/first-post",
+		title: "Netflix Clone : Nebflix",
+		description: `ReactJS, Typescript, Sytled Components, Framer Motion, Recoil, React Query, React Hook Form, Github Pages`,
+		imgSrc: "/static/images/nebflix.jpeg",
+		href: "https://geonya.github.io/nebflix/",
+	},
+	{
+		title: "Carrot Market Clone : Crab Market",
+		description: `NextJS, TailwindCSS, Prisma, React18, Typescript, PlanetScale, Vercel, Cloudflare`,
+		imgSrc: "/static/images/crabmarket.png",
+		href: "https://crab-market.vercel.app/",
 	},
 ];
 
