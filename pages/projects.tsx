6c22719e659882ea0e20eff412e6e8025bb471a8
geonya <geony8410@gmail.com>
2022-04-29 22:49:11 +0900

    220429

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -16,7 +16,7 @@ export default function Projects() {
 						Projects
 					</h1>
 					<p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-						These are the little projects I'm working on!
+						The little projects I'm working on!
 					</p>
 				</div>
 				<div className="container py-12">
